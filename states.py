396bbdb20b2154e8920dc5c7bcca5f3d02165120
Victor Bertollo <bertollo.victor@gmail.com>
2023-10-04 19:16:01 +0300

    Remove states because of useless

--- states.py
+++ states.py
@@ -1,9 +1,1 @@
 from aiogram.fsm.state import State, StatesGroup
-
-class SetDayWord(StatesGroup):
-    channel = State()
-    word = State()
-    all = State()
-
-class Play(StatesGroup):
-    running = State()
