c1eddfcd0449f0c6c5a93c6f49b7da95fda13bb4
gunship <43451657+gunsh1p@users.noreply.github.com>
2023-10-07 17:54:01 +0300

    I18n (#6)
    * Add I18n and create locales
    
    * Add translation for replymarkup
    
    * Add translation for inlinemarkup

--- bot/keyboards/buy_menu.py
+++ bot/keyboards/buy_menu.py
@@ -1,10 +1,18 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.utils.i18n import gettext as _
 
 from utils import goods
 
 def get_buy_menu_keyboard() -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
     for good in goods.get():
-        builder.row(InlineKeyboardButton(text=f"{good['title']} - {good['price']['en']}$/{good['price']['ru']}руб.", callback_data=good['callback']))
+        builder.row(InlineKeyboardButton(
+            text=_("{title} - {price_en}$/{price_ru}rub.").format(
+                title=good['title'],
+                price_en=good['price']['en'],
+                price_ru=good['price']['ru']
+                ), 
+            callback_data=good['callback'])
+        )
     return builder.as_markup()
--- bot/main.py
+++ bot/main.py
@@ -1,9 +1,11 @@
 import asyncio
 import logging
 import sys
+from pathlib import Path
 
 from aiogram import Bot, Dispatcher, enums
 from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.utils.i18n import I18n, SimpleI18nMiddleware
 
 from handlers.commands import register_commands
 from handlers.messages import register_messages
@@ -18,8 +20,14 @@ def setup_routers():
     register_commands(glv.dp)
     register_messages(glv.dp)
 
+def setup_middlewares():
+    i18n = I18n(path=Path(__file__).parent / 'locales', default_locale='en', domain='bot')
+    i18n_middleware = SimpleI18nMiddleware(i18n=i18n)
+    i18n_middleware.setup(glv.dp)
+
 def main():
     setup_routers()
+    setup_middlewares()
     
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(glv.dp.start_polling(glv.bot))
