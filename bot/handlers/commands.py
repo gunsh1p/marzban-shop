218639fb156a31dc0044369b6869f8385b1a0234
Victor Bertollo <bertollo.victor@gmail.com>
2023-10-08 11:01:12 +0300

    Create database models and create database functionality

--- bot/handlers/commands.py
+++ bot/handlers/commands.py
@@ -1,11 +1,16 @@
+import hashlib
+
 from aiogram import Router, F
 from aiogram import Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
 from aiogram.utils.i18n import gettext as _
 from aiogram.utils.i18n import lazy_gettext as __
+from sqlalchemy import select, insert
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from keyboards import get_main_menu_keyboard
+from db.models import VPNUsers
 import glv
 
 router = Router(name="commands-router") 
@@ -13,7 +18,12 @@ router = Router(name="commands-router")
 @router.message(
     Command("start")
 )
-async def start(message: Message):
+async def start(message: Message, session: AsyncSession):
+    await session.merge(VPNUsers(
+        tg_id=message.from_user.id, 
+        vpn_id=hashlib.md5(str(message.from_user.id).encode()).hexdigest())
+    )
+    await session.commit()
     text = _("Hello, {name}.\n\n🎉Welcome to {title}\n\n⬇️Select an action").format(
         name=message.from_user.first_name,
         title=glv.config.get('NAME', 'VPN Shop')
--- bot/main.py
+++ bot/main.py
@@ -6,10 +6,14 @@ from pathlib import Path
 from aiogram import Bot, Dispatcher, enums
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.utils.i18n import I18n, SimpleI18nMiddleware
+from aiogram.utils.callback_answer import CallbackAnswerMiddleware
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 from handlers.commands import register_commands
 from handlers.messages import register_messages
 from handlers.callbacks import register_callbacks
+from middlewares import DbSessionMiddleware
+from db.base import Base
 import glv
 
 glv.bot = Bot(glv.config['BOT_TOKEN'], parse_mode=enums.ParseMode.HTML)
@@ -27,9 +31,21 @@ def setup_middlewares():
     i18n_middleware = SimpleI18nMiddleware(i18n=i18n)
     i18n_middleware.setup(glv.dp)
 
+async def init_models(engine):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+def setup_database():
+    engine = create_async_engine(url=glv.config['DB_URL'], echo=True)
+    asyncio.run(init_models(engine))
+    sessionmaker = async_sessionmaker(engine, expire_on_commit=False)
+    glv.dp.update.middleware(DbSessionMiddleware(session_pool=sessionmaker))
+    glv.dp.callback_query.middleware(CallbackAnswerMiddleware())
+
 def main():
     setup_routers()
     setup_middlewares()
+    setup_database()
     
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(glv.dp.start_polling(glv.bot))
